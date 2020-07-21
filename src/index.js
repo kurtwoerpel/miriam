487894c518452bd2c6175d9a0ad9309702e9fecd
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-07-21 12:52:14 -0400

    first real commit :)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+	
+import './fonts/BrownPro/BrownPro-Regular.otf'	
+import './fonts/JBaskerville/JBaskervilleText.otf'
 
 ReactDOM.render(
   <React.StrictMode>
