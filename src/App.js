f056edfac3f5c5df833d0a7e72e5a51a2f0575dd
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-07-21 16:51:20 -0400

    testing error

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ render() {
     <div className="App">
       <Switch>
         <Route path="/">
-                <Home/>
+             <div>hello</div>
         </Route>
       </Switch>
     </div>
