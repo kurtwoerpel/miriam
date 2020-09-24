5bb38177e37746ceccbbe41877d91e3a69248fd8
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-09-24 01:00:57 -0400

    mobile footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -27,19 +27,19 @@ class Footer extends Component {
 
      <footer style={this.props.style}>
      <div className='row'>
-        <div className='col-3'>
+        <div className='col-6 col-sm-3'>
           <h1 className='text-small'>Miriam</h1>
           <p className='text-tiny baskerville'>gallery + bookshop<br/>@miriam.gallery</p>
         </div>
-        <div className='col-3'>
+        <div className='col-6 col-sm-3'>
           <p className='text-tiny baskerville'>319 Bedford Ave,<br/>Williamsburg<br/>Brooklyn, NY 11211</p>
         </div>
-        <div className='col-3'>
+        <div className='col-6 col-sm-3'>
             <p className='text-tiny baskerville'>
             Mon-Fri: By Appointment<br/>Sat-Sun: 8PM<br/>Always Social Distancing
             </p>
         </div>
-        <div className='col-3'>
+        <div className='col-6 col-sm-3'>
             <p className='text-tiny baskerville'><a>Join Our Mailing List</a><br/>info@miriamgallery.com</p>
         </div>
         </div>
