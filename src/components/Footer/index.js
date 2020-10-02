40911090138d1df1a6f297c6071c21afd11c8545
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-10-02 15:31:50 -0400

    fix bear; "

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -29,7 +29,7 @@ class Footer extends Component {
      <div className='row'>
         <div className='col-6 col-sm-3'>
           <h1 className='text-small'>Miriam</h1>
-          <p className='text-tiny baskerville'>hello@miriam.gallery</p>
+          <p className='text-tiny baskerville'>gallery + bookshop<br/>@miriam.gallery</p>
         </div>
         <div className='col-6 col-sm-3'>
           <p className='text-tiny baskerville'>319 Bedford Ave,<br/>Williamsburg<br/>Brooklyn, NY 11211</p>
@@ -40,7 +40,7 @@ class Footer extends Component {
             </p>
         </div>
         <div className='col-6 col-sm-3'>
-            <p className='text-tiny baskerville'><a href='https://mailchi.mp/0a822e78bfa8/miriammailinglist'>Join Our Mailing List</a><br/>info@miriamgallery.com</p>
+            <p className='text-tiny baskerville'><a href='https://mailchi.mp/0a822e78bfa8/miriammailinglist'>Join Our Mailing List</a><br/>hello@miriamgallery.com</p>
         </div>
         </div>
       </footer>
